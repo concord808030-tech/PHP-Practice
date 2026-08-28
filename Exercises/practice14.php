e63e9c423db640cc164d219e958d53f3746cdc8a
Miguel <concord808030@gmail.com>
2026-08-28 15:51:58 -0800

    NewFilesV2

--- Exercises/practice14.php
+++ Exercises/practice14.php
@@ -5,6 +5,11 @@
 </head>
 <body>
     <?php
+    // Normally, when a function finishes executing, all of its variables are deleted. 
+    // However, sometimes we want a local variable NOT to be deleted. We need it for a further job. To achieve this, use the 
+    // static keyword when you first declare the variable.
+    // Then, each time the function is called, that variable will have the value from the 
+    //last time the function was called.
     function myTest() {
         static $x = 0;
         echo $x;
